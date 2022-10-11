a98d3ac00bffe73e54d3b97e8f4ded2cbe627908
Immanuel Peter <ipmaster40@gmail.com>
2022-10-11 15:06:24 -0700

    Nice nd very easy to read well

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+/*
+ * File: 2-print_dog.c
+ * Auth: Jan Nelson
+ */
+
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - Prints a struct dog.
+ * @d: The struct dog to be printed.
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		;
+	else
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %f\n", d->age);
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory contains files that describe more about preprocessors.
