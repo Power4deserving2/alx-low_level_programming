f568b3046b6c9b79ecab428e03f9961ca09b9dfe
Immanuel Peter <ipmaster40@gmail.com>
2023-06-08 02:12:26 -0700

    this is now the correction switching

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,24 +1,29 @@
 /*
+ * 3-factorial.c
  * Auth: Immanuel Peter
- * Project: 3_factorial.c
  */
-include "main.h"
-/*
- * factorial - Returns the factorial of a number.
- * @n - This is the number to be factorised.
+
+#include "main.h"
+
+/**
+ * factorial - Returns the factorial of a given number.
+ * @n: The number to find the factorial of.
  *
- * return; if n < 0 it returns -1 to show it failed
- * if n > 0 it finds the factorial
+ * Return: If n > 0 - the factorial of n.
+ *         If n < 0 - 1 to indicate an error.
  */
-int factorial(int n);
+int factorial(int n)
 {
+	int result = n;
+
 	if (n < 0)
 		return (-1);
 
 	else if (n >= 0 && n <= 1)
 		return (1);
 
-	else
-		return (n *= factorial(n - 1))
+	result *= factorial(n - 1);
+
+	return (result);
 
 }
---
+++ 0x08-recursion/3_factorial.c
@@ -0,0 +1,22 @@
+/*
+ * Auth: Immanuel Peter
+ * Project: 3_factorial.c
+ */
+include "main.h"
+/*
+ * factorial - Returns the factorial of a number.
+ * @n - This is the number to be factorised.
+ *
+ * return; if n < 0 it returns -1 to show it failed
+ * if n > 0 it finds the factorial
+ */
+int factorial(int n);
+{
+	if (n < 0)
+		return (-1);
+
+	else if (n >= 0 && n <= 1)
+		return (1);
+
+	return (n *= factorial(n - 1))
+}
